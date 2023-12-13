b4cd864bd135d7865073756e20484720e422a1c9
bakhwee-bug <parksy8103@gmail.com>
2023-12-14 04:30:19 +0900

    필요한 액티비티 생성

---
+++ MyHealth/app/src/main/java/com/example/myhealth/IntakeRecordActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myhealth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IntakeRecordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intake_record)
+    }
+}
