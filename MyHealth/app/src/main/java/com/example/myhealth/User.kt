0f84ca7282927428ce5616c1b2688aad9ad3be60
bakhwee-bug <parksy8103@gmail.com>
2023-12-16 04:42:47 +0900

    [로그인액티비티]레이아웃 수정

---
+++ MyHealth/app/src/main/java/com/example/myhealth/User.kt
@@ -0,0 +1,26 @@
+package com.example.myhealth
+
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+data class LoginRequest(
+    @SerializedName("userName") val userName: String,
+    @SerializedName("password") val password: String
+)
+
+data class ResponseData(
+    val code: Int,
+    val httpStatus: String,
+    val message: String,
+    val data: Data
+): Serializable
+
+data class Data(
+    @SerializedName("token") val token: String
+)
+
+data class User(
+    val userName: String,
+    val password: String,
+    val nickName: String
+): Serializable
