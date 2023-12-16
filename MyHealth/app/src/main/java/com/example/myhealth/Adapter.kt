e06a06f98bfce34c1042d13f60fce8cd1c0f6328
bakhwee-bug <parksy8103@gmail.com>
2023-12-17 06:05:06 +0900

    [리사이클러뷰]어댑터 생성

--- MyHealth/app/src/main/java/com/example/myhealth/Adapter.kt
+++ MyHealth/app/src/main/java/com/example/myhealth/Adapter.kt
@@ -1,20 +1,46 @@
 package com.example.myhealth
 
-import android.content.Context
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.core.text.HtmlCompat
-import androidx.recyclerview.widget.RecyclerView.Adapter
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myhealth.databinding.ItemRecyclerAddBinding
 
+class MyAdapter(private val dataList: ArrayList<FoodData>) :
+    RecyclerView.Adapter<MyAdapter.ViewHolder>() {
 
+    val TAG = "MyAdapter"
 
-/*
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateFood(newFood : List<FoodData>){
+        dataList.clear()
+        dataList.addAll(newFood)
+        notifyDataSetChanged()
+    }
+    //생성된 뷰 홀더에 값 정
+    class ViewHolder(val binding: ItemRecyclerAddBinding) :
+        RecyclerView.ViewHolder(binding.root){
 
+            fun bind(currentFood: FoodData){
+                binding.foodTitle.text=currentFood.descKor
+            }
+        }
+    //어떤 xml으로 뷰홀더 생성할지 지정
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemRecyclerAddBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
+    }
 
-class MyAdapter(var context: Context, var items:List<FoodDetailResponse>):Adapter<MyAdapter.VH>(){
+    //뷰 홀더에 데이터 바인딩
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val food = dataList
+        if(food!=null){
+            holder.bind(food.get(position))
+        }
+    }
 
-
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
 }
-*/
