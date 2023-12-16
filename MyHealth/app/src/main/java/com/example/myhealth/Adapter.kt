662d9c0cb3bb271908da21c4d2e4b5831adbf41b
bakhwee-bug <parksy8103@gmail.com>
2023-12-17 07:33:18 +0900

    [리사이클러뷰]식품 데이터 리사이클러뷰로 받아오기

--- MyHealth/app/src/main/java/com/example/myhealth/Adapter.kt
+++ MyHealth/app/src/main/java/com/example/myhealth/Adapter.kt
@@ -1,46 +1,43 @@
 package com.example.myhealth
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.text.HtmlCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myhealth.databinding.ItemRecyclerAddBinding
 
-class MyAdapter(private val dataList: ArrayList<FoodData>) :
-    RecyclerView.Adapter<MyAdapter.ViewHolder>() {
-
-    val TAG = "MyAdapter"
-
-    @SuppressLint("NotifyDataSetChanged")
-    fun updateFood(newFood : List<FoodData>){
-        dataList.clear()
-        dataList.addAll(newFood)
-        notifyDataSetChanged()
+class MyAdapter(var context: Context, var items:MutableList<FoodData>) : RecyclerView.Adapter<MyAdapter.VH>(){
+    //뷰홀더는 아이템뷰 하나짜리 받도록 되어 있음 그래서 binding.root 를 줘야됨
+    inner class VH(var binding: ItemRecyclerAddBinding) : RecyclerView.ViewHolder(binding.root){
     }
-    //생성된 뷰 홀더에 값 정
-    class ViewHolder(val binding: ItemRecyclerAddBinding) :
-        RecyclerView.ViewHolder(binding.root){
-
-            fun bind(currentFood: FoodData){
-                binding.foodTitle.text=currentFood.descKor
-            }
-        }
-    //어떤 xml으로 뷰홀더 생성할지 지정
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val binding = ItemRecyclerAddBinding.inflate(inflater, parent, false)
-        return ViewHolder(binding)
-    }
-
-    //뷰 홀더에 데이터 바인딩
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val food = dataList
-        if(food!=null){
-            holder.bind(food.get(position))
-        }
+    //inflate
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        return VH(ItemRecyclerAddBinding.inflate(LayoutInflater.from(context), parent, false))
     }
 
     override fun getItemCount(): Int {
-        return dataList.size
+        return items.size
     }
-}
+
+    //holder에 연결
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        val item:FoodData = items[position] //배열처럼 쓰는걸 권장
+
+        holder.binding.foodTitle.text = item.descKor
+        holder.binding.foodGram.text = "${item.servingSize}g"
+        holder.binding.foodKcal.text = "${item.nutrCont1}kcal"
+
+/*        //링크 열기
+        holder.binding.root.setOnClickListener {
+            //핸드폰 디바이스의 인터넷 앱을 실행
+            val intent = Intent(Intent.ACTION_VIEW) //인터넷을 보여주는 것
+            intent.data = Uri.parse(item.link)
+            context.startActivity(intent)
+        }*/
+    }
+
+    }
--- MyHealth/app/src/main/java/com/example/myhealth/IntakeAddActivity.kt
+++ MyHealth/app/src/main/java/com/example/myhealth/IntakeAddActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.inputmethod.InputMethodManager
+import android.widget.Adapter
+import android.widget.Toast
 import com.example.myhealth.databinding.ActivityIntakeAddBinding
 import kotlinx.android.synthetic.main.activity_intake_add.*
 import retrofit2.Retrofit
@@ -28,7 +30,7 @@ class IntakeAddActivity : AppCompatActivity() {
         binding.btn.setOnClickListener{
             val searchFoodName = inputFoodName.getText().toString()
             Log.e("IntakeAddActivity", searchFoodName)
-            searchPublicApi(searchFoodName)}
+            searchPublicApi2(searchFoodName)}
     }
 
     fun searchPublicApi(productName: String) {
@@ -69,7 +71,46 @@ class IntakeAddActivity : AppCompatActivity() {
                 }
             })
     }
+    fun searchPublicApi2(productName: String) {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://openapi.foodsafetykorea.go.kr/") // 기본 URL
+            .addConverterFactory(GsonConverterFactory.create()) // JSON 파싱 라이브러리 설정
+            .build()
 
+        val service = retrofit.create(PublicApiService::class.java)
+
+        service.searchByProductName("8152179687f14444a57d", productName) // API 키와 품목명을 입력
+            .enqueue(object : Callback<PublicApiResponce> {
+                override fun onResponse(
+                    call: Call<PublicApiResponce>,
+                    response: Response<PublicApiResponce>
+                ) {
+                    if (response.isSuccessful) {
+                        val data = response.body()
+                        // 성공적으로 데이터를 받았을 때의 처리를 작성
+                        binding.recycler.adapter = MyAdapter(this@IntakeAddActivity, data!!.i2790.row)
+                            // result.text=data.toString()
+                        Log.e("IntakeAddActivity", "값 불러오기 성공")
+                        Log.e("IntakeAddActivity","아이템 개수 : ${data?.i2790?.row?.size}")
+
+                    }else{
+                        //통신 실패
+                        try {
+                            val body = response.errorBody()!!.string()
+                            Log.e("IntakeAddActivity", "error - body : $body")
+                        } catch (e: IOException) {
+                            e.printStackTrace()
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<PublicApiResponce>, t: Throwable) {
+                    // 요청이 실패했을 때의 처리를 작성해주세요.
+                    Log.d("MainActivity_callInbody", "연결 실패")
+
+                }
+            })
+    }
 
 
 }
